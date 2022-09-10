059ec9516a7f2de0cdda9d19cd154e007321faaa
subham singh <shubhsm.singh@lumiq.ai>
2022-09-11 01:55:36 +0530

    object and class concept of oops

---
+++ learning/src/opps/Rect1.java
@@ -0,0 +1,37 @@
+package opps;
+
+public class Rect1 {
+	private int len;
+	private int bre;
+	// getter and setter of square 
+	public int getLen() 
+	{
+		return len;
+	}
+	public void setLen(int len) 
+	{
+		this.len = len;
+	}
+	public int getBre() 
+	{
+		return bre;
+	}
+	public void setBre(int bre) {
+		this.bre = bre;
+	}
+	
+	public int getArea()
+	{
+		return((len*bre));
+	}
+	
+	public int getPerimetre()
+	{
+		return((2*(bre+len)));
+	}
+	
+	
+	
+	
+
+}
---
+++ learning/src/opps/Rectangle.java
@@ -0,0 +1,27 @@
+package opps;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		
+		
+		Rect1 r1= new Rect1();
+		
+		r1.setLen(10);
+		r1.setBre(20);
+		
+		System.out.println(r1.getLen());
+		System.out.println(r1.getBre());
+		
+		System.out.println("Area of rectange= "+ r1.getArea());
+		
+		
+		System.out.println("perimetre of rectange= "+ r1.getPerimetre());
+		
+		
+		
+		
+
+	}
+
+}
