94f68ee7b13caf6f1efb6d60faac459c32858255
subham singh <shubhsm.singh@lumiq.ai>
2022-09-10 22:54:16 +0530

    my 1st technical code on git hub

---
+++ learning/src/learning1/SumOfTwoNo.java
@@ -0,0 +1,13 @@
+package learning1;
+
+public class SumOfTwoNo {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=10;
+		System.out.println("sum of a and b="+ (a+b));
+
+	}
+
+}
