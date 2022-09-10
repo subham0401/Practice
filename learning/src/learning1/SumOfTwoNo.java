af28e11c09bf7860bfee1a0a6c1113c8c079c1f1
subham singh <shubhsm.singh@lumiq.ai>
2022-09-10 23:09:08 +0530

    new changes with same file

--- learning/src/learning1/SumOfTwoNo.java
+++ learning/src/learning1/SumOfTwoNo.java
@@ -7,7 +7,32 @@ public class SumOfTwoNo {
 		int a=10;
 		int b=10;
 		System.out.println("sum of a and b="+ (a+b));
+		
+		int arr[];
+		arr = new int[10];
+		
+		
+		int sum=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			sum+=i;
+			arr[i]=i*10;
+		}
+		
+		System.out.println("sum of 1st ten number = "+ sum);
+		
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			
+			System.out.print("  "+ (arr[i]));
+		}
+		
+		
+		
 
 	}
 
 }
+
+
